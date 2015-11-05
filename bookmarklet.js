5f458c7c5ff59b2d98a021a274eb8bccdfe476ed
Sergey <morozov.sergey@gmail.com>
2015-11-04 22:33:27 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,24 +3,34 @@ javascript:
 var pages = [];
 var stop  = false;
 
-var url  = window.location.href;
-var meta = { method: 'get', parameters: { page: 1 } };
-for(var page = 1; page < 3; page++)
-{
-	meta.parameters.page = page;
-	meta.onSuccess = function (xhr) { get_content(page, xhr.responseText); };
-	new Ajax.Request(url + "?page=" + page, meta);
-	
-	if(stop)
-	  break;
-}
+get_page(1);
 
-alert("Got " + pages.length + "pages");
+function get_page(page)
+{
+	var url  = window.location.href + "?page = " + page;
+	var meta = { method: 'get' };
+	meta.onSuccess = function (xhr) { get_content(page, xhr.responseText); };
+
+	new Ajax.Request(url, meta);
+}
 
 function get_content(page, content)
 {
-	if(page - 1 > 0 && pages[(page - 1)] != content)
+	if(pages.length < 5)
+	{
 		pages[page] = content;
+		
+		
+		
+		get_page(page + 1);
+	}
 	else
-		stop = true;
+	{
+		process_content();
+	}
+}
+
+function process_content()
+{
+	alert("Got " + pages.length + "pages");
 }
