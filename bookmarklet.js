31910aaffc6b8c4a2286a4be5e296998ca19ea4e
Sergey <morozov.sergey@gmail.com>
2015-11-04 21:55:14 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -19,7 +19,6 @@ alert("Got " + pages.length + "pages");
 
 function get_content(page, content)
 {
-  // as long as its different from the previous page (non-existent page = last page)
   if(page - 1 > 0 && pages[(page - 1)] != content)
     pages[page] = content;
   else
