5722b0054807c00fbb555b14f286d86a76a95435
Sergey <morozov.sergey@gmail.com>
2015-11-05 00:05:13 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,11 @@
 javascript:
 
-var pages = [];
-var stop  = false;
-
+var posts = [];
 get_page(1);
 
 function get_page(page)
 {
-	var url  = window.location.href + "?page = " + page;
+	var url  = window.location.href + "?page=" + page;
 	var meta = { method: 'get' };
 	meta.onSuccess = function (xhr) { get_content(page, xhr.responseText); };
 
@@ -16,21 +14,35 @@ function get_page(page)
 
 function get_content(page, content)
 {
-	if(pages.length < 5)
+	var total = content.match(/Pages \((\d+)\)/i)[1];
+	if(page < total)
 	{
-		pages[page] = content;
-		
-		
-		
+		var matches = content.split(/<!-- (start|end): postbit -->/i);
+		matches = matches.filter(function(v){ return v.length > 5; });
+		Array.prototype.push.apply(posts, matches);
+
 		get_page(page + 1);
 	}
 	else
-	{
 		process_content();
-	}
 }
 
 function process_content()
 {
-	alert("Got " + pages.length + "pages");
+	var head = posts.shift();
+	var tail = posts.pop();
+
+	posts.sort(function(a, b){ return get_likes(b) - get_likes(a); });
+
+	document.write("ALL POSTS SORTED BY DESCENDING LIKES");
+	document.write(head);
+	for(var i = 0; i < posts.length; i++)
+		document.write(posts[i]);
+	document.write(tail);
+}
+
+function get_likes(content)
+{
+	var matches = content.match(/The following (\d+) users Like/i);
+	return (matches != null)?matches[1]:0;
 }
